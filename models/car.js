f8b36e5d9a15f30ac5c60ce7c1fd3729bc50d004
Noah Murphy <ncm0897@gmail.com>
2024-07-25 16:05:56 -0700

    Add the .env with MongoDB set up as well as all the other dependencies, create the landing page, as well as the method to go to the form to put in a new car

---
+++ models/car.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const carSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    isRunning: Boolean 
+})
+
+const Car = mongoose.model('Car', carSchema)
+
+module.exports = Car
---
+++ server.js
@@ -0,0 +1,50 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.on('connected', () => {
+    console.log(`connected to MongoDB ${mongoose.connection.name}`)
+})
+
+
+const Car = require('./models/car.js')
+
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.get('/cars/new', (req, res) => {
+    res.render('cars/new.ejs')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
