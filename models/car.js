984fae4c8af483455862f137ae10a70671038040
Noah Murphy <ncm0897@gmail.com>
2024-07-25 16:55:10 -0700

    add cars index and new ejs and create routes to each. GET and POST methods complete

--- models/car.js
+++ models/car.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const carSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
-    isRunning: Boolean 
+    isRunning: Boolean,
 })
 
 const Car = mongoose.model('Car', carSchema)
--- server.js
+++ server.js
@@ -15,12 +15,30 @@ mongoose.connection.on('connected', () => {
 const Car = require('./models/car.js')
 
 
+app.use(express.urlencoded({ extended: false }))
+
+
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
+app.get('/cars', async (req, res) => {
+    const allCars = await Car.find({})
+    res.render('cars/index.ejs', { cars: allCars })
+})
+
 app.get('/cars/new', (req, res) => {
     res.render('cars/new.ejs')
+})
+
+app.post('/cars', async (req, res) => {
+    if (req.body.isRunning === 'on') {
+        req.body.isRunning = true
+    } else {
+        req.body.isRunning = false
+    }
+    await Car.create(req.body)
+    res.redirect('/cars')
 })
 
 
